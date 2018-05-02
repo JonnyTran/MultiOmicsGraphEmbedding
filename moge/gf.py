98f582c1ff9dc102139e78bc6bc480ee00d029cc
JonnyTran <Dumamay69>
2018-05-02 17:17:47 -0500

    matrix factorization running

--- moge/gf.py
+++ moge/gf.py
@@ -30,19 +30,19 @@ class GraphFactorization():
                             validate_shape=True, dtype=tf.float32,
                             name="z_emb", trainable=True)
 
-        z_ij_inner = tf.matmul(tf.transpose(tf.slice(z_emb, [i, 0], [self.d, 1], name="slice1"), name="slice1_T"),
-                                                            tf.slice(z_emb, [j, 0], [self.d,1 ], name="slice2"),
-                               name="z_ij_inner")
+        z_ij_inner = tf.matmul(tf.slice(z_emb, [i, 0], [1, z_emb.get_shape()[1]], name="slice1"),
+                               tf.slice(z_emb, [j, 0], [1, z_emb.get_shape()[1]], name="slice2"),
+                               name="z_ij_inner", transpose_b=True)
 
         regu_term = lr/2.0 * tf.square(tf.reduce_mean(z_emb[i]), name="regu_term")
 
         # Loss Function: 1/2 * sum_ij (Y_ij - <Z_i, Z_j>)^2 + lr/2 * sum_i |Z_i|^2
-        loss = 1.0/2.0 * tf.add(tf.square(tf.reduce_mean(y_ij - z_ij_inner)), regu_term, name="loss")
-
+        loss = 1.0/2.0 * tf.add(tf.reduce_mean(y_ij - z_ij_inner), regu_term, name="loss")
 
         with tf.Session() as session:
             session.run(tf.global_variables_initializer())
-            print('Loss(x,y) = %.3f' % session.run(z_ij_inner, {i:0, j:1, y_ij:[1,]}))
+            print('Loss(x,y) = %.3f' % session.run(z_ij_inner, {i:2, j:3, y_ij:[1,]}))
+            print(tf.slice(z_emb, [j, -1], [self.d, 0]).get_shape())
 
         # Add the loss value as a scalar to summary.
         tf.summary.scalar('loss', loss)
@@ -57,15 +57,10 @@ class GraphFactorization():
         with tf.Session() as session:
             session.run(init_op)
             average_loss = 0
-            count = 0
             for step in range(iterations):
-                print(step, count)
-                count = 0
-
+                print("iteration", step)
                 rows, cols = Y.nonzero()
                 for x,y in zip(rows,cols):
-                    count+=1
-
                     feed_dict = {y_ij: [Y[x, y],],
                                  i: x,
                                  j: y}
@@ -80,13 +75,12 @@ class GraphFactorization():
 
 if __name__ == '__main__':
 
-    ppi = pd.read_table("/home/jonny_admin/PycharmProjects/nuclei-segmentation/notebooks/gem/data/BINARY_PROTEIN_PROTEIN_INTERACTIONS.txt", header=None)
-    # ppi.filter([0, 3])
+    G = nx.read_edgelist("/Users/jonny/Desktop/PycharmProjects/MultiOmicsGraphEmbedding/data/karate.edgelist", create_using=nx.DiGraph())
 
-    G = nx.from_pandas_dataframe(ppi, source=0, target=3, create_using=nx.DiGraph())
+    # G = nx.from_pandas_edgelist(ppi, source=0, target=3, create_using=nx.DiGraph())
     # nx.relabel.convert_node_labels_to_integers(G)
 
-    gf = GraphFactorization(G, d=200, reg=1.0, lr=1.0)
+    gf = GraphFactorization(G, d=100, reg=1.0, lr=1.0)
     Y = nx.adjacency_matrix(G)
 
     gf.train(Y=Y)
