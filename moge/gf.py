a7971a9bcfdeec408d48ca7b39a134bb8d65f68a
JonnyTran <JonnyTran@github.com>
2018-05-01 21:38:12 -0500

    fixed inputs problems

--- moge/gf.py
+++ moge/gf.py
@@ -16,8 +16,8 @@ class GraphFactorization():
     def train(self, Y, iterations=100, batch_size=1):
         with tf.name_scope('inputs'):
             y_ij = tf.placeholder(tf.float32, shape=(1, ), name="y_ij")
-            i = tf.Variable(int, name="i")
-            j = tf.Variable(int, name="j")
+            i = tf.Variable(int, name="i", trainable=False)
+            j = tf.Variable(int, name="j", trainable=False)
 
         # y_ij = tf.Variable(name="y_ij")
         #
@@ -26,26 +26,33 @@ class GraphFactorization():
 
         lr = tf.constant(self.lr)
 
-        z_emb = tf.Variable(initial_value=tf.random_uniform(
-                [self.n_nodes, self.d], -1, 1),
-            name="z_emb")
+        z_emb = tf.Variable(initial_value=tf.random_uniform([self.n_nodes, self.d], -1, 1), validate_shape=True,
+                            name="z_emb", trainable=True)
 
-        z_ind = tf.range(self.d)
+        z_ij_inner = tf.matmul(tf.transpose(tf.slice(z_emb, [i, 0], [self.d, 1])),
+                                                            tf.slice(z_emb, [j, 0], [self.d, 1]))
+
+        regu_term = lr/2.0 * tf.square(tf.reduce_mean(z_emb[i]))
 
         # Loss Function: 1/2 * sum_ij (Y_ij - <Z_i, Z_j>)^2 + lr/2 * sum_i |Z_i|^2
-        loss = 1.0/2.0 * tf.square(tf.reduce_mean(y_ij - \
-                                                  tf.matmul(tf.transpose(tf.slice(z_emb, [i, 0], [self.d, 1])),
-                                                            tf.slice(z_emb, [j, 0], [self.d, 1])))) + \
-               lr/2.0 * tf.square(tf.reduce_mean(z_emb[i]))
+        loss = 1.0/2.0 * tf.square(tf.reduce_mean(y_ij - z_ij_inner)) + regu_term
+
+        with tf.Session() as session:
+            session.run(tf.global_variables_initializer())
+            print('Loss(x,y) = %.3f' % session.run(tf.reduce_mean(z_emb[i]), {i:0, j:1, y_ij:[1,]}))
 
         # Add the loss value as a scalar to summary.
         tf.summary.scalar('loss', loss)
         merged = tf.summary.merge_all()
 
+        # Initialize variables
+        init_op = tf.global_variables_initializer()
+
         # SGD Optimizer
-        self.optimizer = tf.train.GradientDescentOptimizer(self.lr).minimize(loss)
+        self.optimizer = tf.train.GradientDescentOptimizer(self.lr).minimize(loss, var_list=[z_emb])
 
         with tf.Session() as session:
+            session.run(init_op)
             average_loss = 0
             count = 0
             for step in range(iterations):
@@ -55,6 +62,7 @@ class GraphFactorization():
                 rows, cols = Y.nonzero()
                 for x,y in zip(rows,cols):
                     count+=1
+
                     feed_dict = {y_ij: [Y[x, y],],
                                  i: x,
                                  j: y}
@@ -65,6 +73,7 @@ class GraphFactorization():
 
                     average_loss += loss_val
 
+        print(average_loss)
 
 if __name__ == '__main__':
 
@@ -74,7 +83,7 @@ if __name__ == '__main__':
     G = nx.from_pandas_dataframe(ppi, source=0, target=3, create_using=nx.DiGraph())
     # nx.relabel.convert_node_labels_to_integers(G)
 
-    gf = GraphFactorization(G, d=100, reg=1.0, lr=1.0)
+    gf = GraphFactorization(G, d=200, reg=1.0, lr=1.0)
     Y = nx.adjacency_matrix(G)
 
     gf.train(Y=Y)
