3b6e50996cda6d5bbd45e9b33cbb874c2db87296
JonnyTran <Dumamay69>
2018-09-18 01:16:41 -0500

    refactored data_generator

--- moge/evaluation/utils.py
+++ moge/evaluation/utils.py
@@ -51,8 +51,6 @@ def split_train_test_nodes(network:HeterogeneousNetwork, node_list, edge_types=[
     print(network.G.number_of_nodes())
     network_train, val_edges, test_edges = mask_test_nodes(network, node_list,
                                                            test_frac=test_frac, val_frac=val_frac, seed=seed)
-    print(test_edges)
-    print(network_train.number_of_nodes())
     for edge_type in edge_types:
         test_edges_dict[edge_type] = [(u, v) for u, v, d in test_edges if d["type"] == edge_type]
         val_edges_dict[edge_type] = [(u, v) for u, v, d in val_edges if d["type"] == edge_type]
