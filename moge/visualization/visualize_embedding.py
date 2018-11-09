cc3e4f0cca8bc5b315fa19134e3a246f906d3398
JonnyTran <Dumamay69>
2018-11-09 00:22:16 -0600

    can query top k predicted edges

--- moge/visualization/visualize_embedding.py
+++ moge/visualization/visualize_embedding.py
@@ -5,9 +5,10 @@ import networkx as nx
 from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 from MulticoreTSNE import MulticoreTSNE as TSNE
+import random
 
 
-def visualize_embedding(embedding, network_train, edgelist=None, node_label="locus_type"):
+def visualize_embedding(embedding, network_train, edgelist=None, node_label="locus_type", cmap="viridis"):
     nodelist = embedding.node_list
     embs = embedding.get_embedding()
     embs_pca = PCA(n_components=2).fit_transform(embs)
@@ -23,18 +24,18 @@ def visualize_embedding(embedding, network_train, edgelist=None, node_label="loc
         node_with_lable = genes_info[node_label].notnull().index
         nodelist = [node for node in nodelist if node in node_with_lable]
         node_labels = genes_info.loc[nodelist][node_label].astype(str)
-        sorted_node_labels = sorted(node_labels.unique(), reverse=True)
+        sorted_node_labels = sorted(node_labels.unique(), reverse=random.choice([True, False]))
         node_colormap = {f: sorted_node_labels.index(f) / len(sorted_node_labels) for f in node_labels.unique()}
         node_colors = [node_colormap[n] if n in node_colormap.keys() else None for n in node_labels]
 
         plot_embedding2D(node_pos, node_list=embedding.node_list, node_colors=node_colors,
                          legend=True, node_labels=node_labels, node_colormap=node_colormap, legend_size=20,
-                         di_graph=network_train.G, cmap="gist_ncar", nodelist=nodelist,
+                         di_graph=network_train.G, cmap=cmap, nodelist=nodelist,
                          plot_nodes_only=False, edgelist=edgelist,
                          with_labels=False, figsize=(20, 15))
     else:
         plot_embedding2D(node_pos, node_list=embedding.node_list,
-                         di_graph=network_train.G, cmap="gist_ncar", nodelist=nodelist,
+                         di_graph=network_train.G, cmap=cmap, nodelist=nodelist,
                          plot_nodes_only=False, edgelist=edgelist,
                          with_labels=False, figsize=(20, 15))
 
