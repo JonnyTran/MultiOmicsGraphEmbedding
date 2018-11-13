a75a8383e748d84f2b7c09d11b0c54e98c40f751
JonnyTran <Dumamay69>
2018-11-12 20:37:26 -0600

    node classification

--- moge/evaluation/node_classification.py
+++ moge/evaluation/node_classification.py
@@ -1,11 +1,12 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MultiLabelBinarizer, LabelBinarizer
-from sklearn.model_selection import StratifiedKFold
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import cross_validate
+from sklearn.preprocessing import MultiLabelBinarizer, LabelBinarizer
 from sklearn import svm
 
 
-def evaluate_classification(embedding, network, node_label="Family", cv=5,
+def evaluate_classification(embedding, network, node_label="Family", cv=5, multilabel=False,
                             scoring=['precision_macro', 'recall_macro']):
     nodelist = embedding.node_list
     genes_info = network.genes_info
@@ -14,9 +15,16 @@ def evaluate_classification(embedding, network, node_label="Family", cv=5,
     node_groups = genes_info.loc[nodelist, "locus_type"]
 
     X = embedding.get_embedding()
-    y = genes_info.loc[nodelist, node_label].str.split("|", expand=True)[0]
+    if multilabel:
+        labels = genes_info.loc[nodelist, node_label].str.split("|", expand=False)
+        labeler = MultiLabelBinarizer()
+        y = labeler.fit_transform(labels.tolist())
+        clf = KNeighborsClassifier(n_neighbors=10, weights="distance", algorithm="auto", metric="euclidean")
 
-    clf = svm.LinearSVC(multi_class="crammer_singer")
+    else:  # Multiclass classification (only single label each sample)
+        y = genes_info.loc[nodelist, node_label].str.split("|", expand=True)[0]
+        clf = svm.LinearSVC(multi_class="ovr")
+
     scores = cross_validate(clf, X, y, groups=node_groups, cv=cv, n_jobs=-2, scoring=scoring)
 
     return scores
