15fb8e494734e8d020292d7667a4c3073bba9555
JonnyTran <Dumamay69>
2018-11-12 19:05:25 -0600

    clustering

--- moge/evaluation/node_clustering.py
+++ moge/evaluation/node_clustering.py
@@ -1,15 +1,19 @@
 from sklearn.metrics import homogeneity_score, completeness_score, normalized_mutual_info_score
 
 
-def evaluate_clustering(embedding, network, node_label="locus_type", n_clusters=8,
+def evaluate_clustering(embedding, network, node_label="locus_type", n_clusters=None,
                         metrics=["homogeneity", "nmi", "completeness"]):
     nodelist = embedding.node_list
     genes_info = network.genes_info
     nodes_with_label = genes_info[genes_info[node_label].notna()].index
 
     nodelist = [node for node in nodelist if node in nodes_with_label]
-    y_pred = embedding.predict_cluster(n_clusters, node_list=nodelist)
     y_true = genes_info.loc[nodelist, node_label]
+
+    if n_clusters is None:
+        n_clusters = len(y_true.unique())
+    y_pred = embedding.predict_cluster(n_clusters, node_list=nodelist)
+    print(y_true[0:5], y_pred[:5])
     assert len(y_pred) == len(y_true)
 
     results = {}
