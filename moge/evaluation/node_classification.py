648f754583437ed572a052940fbbba063352d815
JonnyTran <Dumamay69>
2018-12-06 12:40:05 -0600

    switched to KNN

--- moge/evaluation/node_classification.py
+++ moge/evaluation/node_classification.py
@@ -1,11 +1,12 @@
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
 from sklearn.model_selection import cross_validate
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def evaluate_classification(embedding, network, cv=5, node_label="Family", multilabel=False,
+def evaluate_classification(embedding, network, cv=5, node_label="Family", multilabel=False, classifier=None,
                             scoring=['precision_micro', 'recall_micro', "f1_micro"], verbose=False):
     nodelist = embedding.node_list
     genes_info = network.genes_info
@@ -37,6 +38,9 @@ def evaluate_classification(embedding, network, cv=5, node_label="Family", multi
     else:  # Multiclass classification (only single label each sample)
         y = genes_info.loc[nodelist, node_label].str.split("|", expand=True)[0]
         clf = svm.LinearSVC(multi_class="ovr")
+
+    if classifier:
+        clf = classifier
 
     scores = cross_validate(clf, X, y, groups=nodes_split_by_group, cv=cv, n_jobs=-2, scoring=scoring,
                             return_train_score=False)
