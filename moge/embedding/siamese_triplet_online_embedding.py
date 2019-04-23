4c211775d27ad5a0d8d06b79bb1f74e49bfabcd3
JonnyTran <jonnytran@github.com>
2019-04-22 22:34:42 -0500

    latest

--- moge/embedding/siamese_triplet_online_embedding.py
+++ moge/embedding/siamese_triplet_online_embedding.py
@@ -79,8 +79,9 @@ class SiameseOnlineTripletGraphEmbedding(SiameseTripletGraphEmbedding):
                                                 margin=self.directed_margin)
         undirected_loss = self.directed_proba * batch_hard_triplet_loss(pairwise_distance_undirected, labels_undirected,
                                                                         margin=self.undirected_margin)
-        loss = directed_loss + undirected_loss
-        print("loss", loss)
+
+        def loss(_y_true, _y_pred):
+            return directed_loss + undirected_loss
         return loss
 
     def batch_contrastive_loss(self, inputs):
