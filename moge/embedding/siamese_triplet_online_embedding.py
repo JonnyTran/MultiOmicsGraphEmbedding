6dfd2162a113c8d8aab7995fc2b5bb1ecd77a026
JonnyTran <Dumamay69>
2019-04-26 22:47:27 -0500

    latest

--- moge/embedding/siamese_triplet_online_embedding.py
+++ moge/embedding/siamese_triplet_online_embedding.py
@@ -93,7 +93,8 @@ class SiameseOnlineTripletGraphEmbedding(SiameseTripletGraphEmbedding):
         y_pred_undirected = tf.gather_nd(pairwise_distance_undirected, labels_undirected.indices)
         y_true_undirected = labels_undirected.values
 
-        undirected_loss = self.directed_proba * binary_crossentropy(y_true_undirected, y_pred_undirected)
+        undirected_loss = self.directed_proba * contrastive_loss(y_true_undirected, y_pred_undirected,
+                                                                 self.undirected_margin)
         undirected_loss = K.switch(tf.is_nan(undirected_loss), 0.0, undirected_loss)
 
         def _contrastive_loss(_y_true, _y_pred):
@@ -124,9 +125,8 @@ class SiameseOnlineTripletGraphEmbedding(SiameseTripletGraphEmbedding):
             dot_product = K.dot(embeddings_s, K.transpose(embeddings_t))
             return K.sigmoid(dot_product)
         else:
-            return _pairwise_cosine_similarity(embeddings, embeddings)
-            # dot_product = K.dot(embeddings, K.transpose(embeddings))
-            # return K.softmax(dot_product)
+            dot_product = K.dot(embeddings, K.transpose(embeddings))
+            return K.sigmoid(dot_product)
 
     def build_keras_model(self, multi_gpu=False):
         if multi_gpu:
@@ -155,7 +155,7 @@ class SiameseOnlineTripletGraphEmbedding(SiameseTripletGraphEmbedding):
 
             directed_pairwise_distances = Lambda(lambda x: self.pairwise_distances(x, directed=True, squared=False),
                                                  name="directed_pairwise_distances")(embeddings)
-            undirected_pairwise_distances = Lambda(lambda x: self.pairwise_similarity(x, directed=False),
+            undirected_pairwise_distances = Lambda(lambda x: self.pairwise_distances(x, directed=False, squared=False),
                                                    name="undirected_pairwise_distances")(embeddings)
             print("directed_pairwise_distances", directed_pairwise_distances) if self.verbose else None
 
