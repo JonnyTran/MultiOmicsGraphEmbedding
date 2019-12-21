bd7048878fcb7184fd6880fb6c7fd2f675ae2598
Jonny Tran <nhat.tran@mavs.uta.edu>
2019-12-20 23:44:06 -0600

    resample nodes if not same as batch_size

--- moge/generator/subgraph_generator.py
+++ moge/generator/subgraph_generator.py
@@ -29,8 +29,7 @@ class SubgraphGenerator(SampledDataGenerator):
         while len(sampled_nodes) < self.batch_size:
             add_nodes = np.random.choice(self.node_list, size=self.batch_size - len(sampled_nodes), replace=False,
                                          p=self.node_sampling_freq).tolist()
-            sampled_nodes.append(add_nodes)
-            print(type(sampled_nodes))
+            sampled_nodes = sampled_nodes + add_nodes
             sampled_nodes = list(OrderedDict.fromkeys(sampled_nodes))
             sampled_nodes = self.annotations.loc[sampled_nodes, self.variables + self.targets].dropna().index.tolist()
 
