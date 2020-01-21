f5fbfcd67d956d230873316803bc1a35a1f27383
JonnyTran <jonnytran@github.com>
2020-01-21 17:07:07 -0600

    refactoring

--- moge/embedding/static_graph_embedding.py
+++ moge/embedding/static_graph_embedding.py
@@ -8,8 +8,8 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.metrics import pairwise_distances
 
+from moge.evaluation.clustering import _get_top_enrichr_term, chunkIt
 from moge.evaluation.link_prediction import largest_indices
-from moge.evaluation.node_clustering import _get_top_enrichr_term, chunkIt
 from moge.evaluation.utils import get_scalefree_fit_score
 
 
--- moge/evaluation/metrics.py
+++ moge/evaluation/metrics.py
@@ -1,7 +1,39 @@
 import keras.backend as K
-import numpy as np
 import tensorflow as tf
-from sklearn.metrics import roc_auc_score, average_precision_score
+
+
+def precision(y_true, y_pred):
+    """Precision metric.
+
+    Only computes a batch-wise average of precision.
+
+    Computes the precision, a metric for multi-label classification of
+    how many selected items are relevant.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def recall(y_true, y_pred):
+    """Recall metric.
+
+    Only computes a batch-wise average of recall.
+
+    Computes the recall, a metric for multi-label classification of
+    how many relevant items are selected.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def f1(y_true, y_pred):
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
 
 
 def precision_d(y_true, y_pred):
@@ -18,19 +50,6 @@ def precision_d(y_true, y_pred):
     precision = true_positives / (predicted_positives + K.epsilon())
     return precision
 
-def precision(y_true, y_pred):
-    """Precision metric for keras models
-
-    Only computes a batch-wise average of precision.
-
-    Computes the precision, a metric for multi-label classification of
-    how many selected items are relevant.
-    """
-    y_pred = K.cast(y_pred >= 0.5, y_true.dtype)
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
-    precision = true_positives / (predicted_positives + K.epsilon())
-    return precision
 
 
 def recall_d(y_true, y_pred):
@@ -48,49 +67,12 @@ def recall_d(y_true, y_pred):
     return recall
 
 
-def recall(y_true, y_pred):
-    """Recall metric for keras models
-
-    Only computes a batch-wise average of recall.
-
-    Computes the recall, a metric for multi-label classification of
-    how many relevant items are selected.
-    """
-    y_pred = K.cast(y_pred > 0.5, y_true.dtype)
-    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
-    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
-    recall = true_positives / (possible_positives + K.epsilon())
-    return recall
-
-def f1(y_true, y_pred):
-    precision = precision(y_true, y_pred)
-    recall = recall(y_true, y_pred)
-    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
-
 
 def accuracy_d(y_true, y_pred):
     ''' Compute classification accuracy with a fixed threshold on distances.
     '''
     return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
 
-
-def auc_roc_d(y_true, y_pred):
-    # any tensorflow metric
-    y_pred = K.exp(-2 * y_pred)
-    value, update_op = tf.contrib.metrics.streaming_auc(y_pred, y_true)
-
-    # find all variables created for this metric
-    metric_vars = [i for i in tf.local_variables() if 'auc_roc' in i.name.split('/')[1]]
-
-    # Add metric variables to GLOBAL_VARIABLES collection.
-    # They will be initialized for new session.
-    for v in metric_vars:
-        tf.add_to_collection(tf.GraphKeys.GLOBAL_VARIABLES, v)
-
-    # force to update metric values
-    with tf.control_dependencies([update_op]):
-        value = tf.identity(value)
-        return value
 
 
 def auc_roc(y_true, y_pred):
@@ -110,62 +92,4 @@ def auc_roc(y_true, y_pred):
         value = tf.identity(value)
         return value
 
-@DeprecationWarning
-def link_prediction_score(true_edges, pred_edges, directed=True, metrics=["precision", "recall"]):
-    """
-    Evaluate link prediction accuracy scores between true_edges, pred_edges. The
 
-    :param true_edges: a list of ground truth edges. Each element is a tuple of two node gene names
-    :param pred_edges: a list of predicted edges. Each element is a tuple of two node gene names
-    :param directed: bool
-    :param metrics: list of metrics ot compute
-    :return:
-    """
-    if directed==False:
-        true_edges = set([(edge[0], edge[1]) for edge in true_edges]).union(set([(edge[1], edge[0]) for edge in true_edges]))
-        pred_edges = set([(edge[0], edge[1]) for edge in pred_edges]).union(
-            set([(edge[1], edge[0]) for edge in pred_edges]))
-
-    true_edges = set(true_edges)
-    pred_edges = set(pred_edges)
-
-    scores = {}
-    if "precision" in metrics:
-        scores["precision"] = float(len(pred_edges & true_edges)) / len(pred_edges)
-
-    if "recall" in metrics:
-        scores["recall"] = float(len(pred_edges & true_edges)) / len(true_edges)
-
-    return scores
-
-
-# Input: positive test/val edges, negative test/val edges, edge score matrix
-# Output: ROC AUC score, ROC Curve (FPR, TPR, Thresholds), AP score
-def get_roc_score(edges_pos, edges_neg, score_matrix):
-    # Edge case
-    if len(edges_pos) == 0 or len(edges_neg) == 0:
-        return (None, None, None)
-
-    # Store positive edge predictions, actual values
-    preds_pos = []
-    pos = []
-    for edge in edges_pos:
-        preds_pos.append(score_matrix[edge[0], edge[1]])
-        pos.append(1)  # actual value (1 for positive)
-
-    # Store negative edge predictions, actual values
-    preds_neg = []
-    neg = []
-    for edge in edges_neg:
-        preds_neg.append(score_matrix[edge[0], edge[1]])
-        neg.append(0)  # actual value (0 for negative)
-
-    # Calculate scores
-    preds_all = np.hstack([preds_pos, preds_neg])
-    labels_all = np.hstack([np.ones(len(preds_pos)), np.zeros(len(preds_neg))])
-    roc_score = roc_auc_score(labels_all, preds_all)
-    # roc_curve_tuple = roc_curve(labels_all, preds_all)
-    ap_score = average_precision_score(labels_all, preds_all)
-
-    # return roc_score, roc_curve_tuple, ap_score
-    return roc_score, ap_score
