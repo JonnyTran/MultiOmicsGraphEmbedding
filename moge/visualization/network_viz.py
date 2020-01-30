34dff458c0cc4a289d09b779b9f5d81e38af8a8e
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-01-30 16:37:52 -0600

    network visualization

--- moge/visualization/network_viz.py
+++ moge/visualization/network_viz.py
@@ -25,28 +25,33 @@ forceatlas2 = ForceAtlas2(
     verbose=False)
 
 
-def graph_viz(g: nx.Graph, nodelist: list, node_labels=None, edge_label=None, title="Graph", pos=None, iterations=100):
+def graph_viz(g: nx.Graph,
+              nodelist: list, node_symbol=None, node_color=None,
+              edge_label=None,
+              title=None,
+              pos=None, iterations=100):
     if pos is None:
         pos = forceatlas2.forceatlas2_networkx_layout(g.subgraph(nodelist), pos=None, iterations=iterations)
-    if node_labels is not None and node_labels.isna().any():
-        node_labels.fillna("nan", inplace=True)
+    if node_symbol is not None and node_symbol.isna().any():
+        node_symbol.fillna("nan", inplace=True)
 
     node_x, node_y = zip(*[(pos[node][0], pos[node][1])
                            for node in nodelist])
     edge_data = pd.DataFrame([{"x": [pos[edge[0]][0], pos[edge[1]][0], None],
                                "y": [pos[edge[0]][1], pos[edge[1]][1], None],
-                               **edge[2]
+                               **edge[2]  # edge d
                                }
                               for edge in g.subgraph(nodelist).edges(data=True)])
 
     fig = px.scatter(x=node_x, y=node_y,
                      hover_name=nodelist,
-                     symbol=node_labels if node_labels is not None else None,
+                     symbol=node_symbol if node_symbol is not None else None,
+                     color=node_color if node_color is not None else None,
                      title=title)
-    fig.add_scatter(x=edge_data["x"].tolist(), y=edge_data["y"].tolist(),
+    fig.add_scatter(edge_data, x="x", y="y",
                     mode='lines',
-                    line=dict(color=edge_data[edge_label].tolist() if edge_label else 'rgb(210,210,210)',
-                              width=1),
+                    line=dict(width=1),
+                    fill=edge_data[edge_label] if edge_label else 'rgb(210,210,210)',
                     hoverinfo='none')
 
     return fig
