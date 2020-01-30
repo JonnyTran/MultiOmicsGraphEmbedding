0db4030ddd8d98640b7245c45337c4efb79e681e
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-01-30 12:52:56 -0600

    network refactorings

---
+++ moge/visualization/network_viz.py
@@ -0,0 +1,61 @@
+import networkx as nx
+import plotly.graph_objects as go
+import plotly.plotly as py
+from igraph.layout import Layout
+
+
+def graph_viz(g: nx.Graph, title="Graph"):
+    pos = nx.spring_layout(g)
+
+    Xv = [pos[k][0] for k in g.nodes]
+    Yv = [pos[k][1] for k in g.nodes]
+    Xed = []
+    Yed = []
+    for edge in g.edges(data=False):
+        Xed += [pos[edge[0]][0], pos[edge[1]][0], None]
+        Yed += [pos[edge[0]][1], pos[edge[1]][1], None]
+
+    edges = go.Scatter(x=Xed,
+                       y=Yed,
+                       mode='lines',
+                       line=dict(color='rgb(210,210,210)', width=1),
+                       hoverinfo='none'
+                       )
+    nodes = go.Scatter(x=Xv,
+                       y=Yv,
+                       mode='markers',
+                       name='net',
+                       marker=dict(symbol='circle-dot',
+                                   size=5,
+                                   color='#6959CD',
+                                   line=dict(color='rgb(50,50,50)', width=0.5)
+                                   ),
+                       text=list(g.nodes),
+                       hoverinfo='text'
+                       )
+
+    layout = Layout(title=title,
+                    font=dict(size=12),
+                    showlegend=False,
+                    autosize=True,
+                    hovermode='closest',
+                    annotations=[
+                        dict(
+                            showarrow=False,
+                            text='This igraph.Graph has the Kamada-Kawai layout',
+                            xref='paper',
+                            yref='paper',
+                            x=0,
+                            y=-0.1,
+                            xanchor='left',
+                            yanchor='bottom',
+                            font=dict(
+                                size=14
+                            )
+                        )
+                    ]
+                    )
+
+    data1 = [edges, nodes]
+    fig1 = go.Figure(data=data1, layout=layout)
+    return py.iplot(fig1)
--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+requirements = [
+    'numpy', 'pandas', 'networkx>=2.1', 'dask', 'biopython', 'bioservices', 'plotly', 'python-igraph',
+]
+
 setup(
     name='MultiOmicsGraphEmbedding',
     version='0.1',
     packages=['moge', 'moge.embedding', 'moge.network', 'moge.evaluation', 'moge.visualization', 'moge.generator'],
+    install_requires=requirements,
     url='',
     license='',
     author='JonnyTran',
