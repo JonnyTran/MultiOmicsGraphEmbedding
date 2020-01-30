11c62813abd34fd7ec13eedb647b1616675238eb
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-01-30 17:47:13 -0600

    network visualization

--- moge/visualization/network_viz.py
+++ moge/visualization/network_viz.py
@@ -90,8 +90,8 @@ def graph_viz(g: nx.Graph,
                      title=title)
     fig.add_scatter(x=edge_data["x"], y=edge_data["y"],
                     mode='lines',
-                    line=dict(color=hash_color(edge_data[edge_label]) if edge_label else 'rgb(210,210,210)',
-                              width=1),
+                    line=dict(width=1),
+                    color=hash_color(edge_data[edge_label]) if edge_label else 'rgb(210,210,210)',
                     hoverinfo='none')
 
     return fig
