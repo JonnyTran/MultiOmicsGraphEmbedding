39e774bb93c10130ddbea4bd62578986ebf89e83
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-03-10 15:38:03 -0500

    dataset

--- moge/generator/subgraph_generator.py
+++ moge/generator/subgraph_generator.py
@@ -42,8 +42,8 @@ class SubgraphGenerator(SampledDataGenerator):
                (tf.float32,) * len(self.variables)
 
     def get_output_shapes(self):
-        return (tf.TensorShape([self.batch_size, self.maxlen]),  # input_seqs
-                tf.TensorShape([self.batch_size, self.batch_size]),  # subnetwork
+        return (tf.TensorShape([self.maxlen]),  # input_seqs
+                tf.TensorShape([None, ]),  # subnetwork
                 ) + \
                (tf.TensorShape([None, ]),) * len(self.variables)
 
