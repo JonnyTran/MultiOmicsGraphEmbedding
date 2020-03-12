f49e9527e82aa7327d77eddffc19914db59340ac
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-03-11 19:13:03 -0500

    added plot_roc_curve

--- moge/visualization/evaluation.py
+++ moge/visualization/evaluation.py
@@ -11,24 +11,12 @@ from .utils import colors
 def plot_roc_curve(y_test, y_score, n_classes, sample_weight=None, width=700, height=700,
                    title='Receiver operating characteristic example'):
     # Compute ROC curve and ROC area for each class
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    for i in range(n_classes):
-        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i],
-                                      sample_weight=sample_weight if sample_weight is not None else None)
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel(),
-                                              sample_weight=sample_weight if sample_weight is not None else None)
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    fpr, roc_auc, tpr = compute_roc_auc_curve(y_test, y_score, range(n_classes), sample_weight)
 
     trace1 = go.Scatter(x=fpr[2], y=tpr[2],
                         mode='lines',
                         line=dict(color='darkorange', width=2),
-                        name='ROC curve (area = %0.2f)' % roc_auc[2]
-                        )
+                        name='ROC curve (area = %0.2f)' % roc_auc[2])
 
     trace2 = go.Scatter(x=[0, 1], y=[0, 1],
                         mode='lines',
@@ -50,18 +38,7 @@ def plot_roc_curve_multiclass(y_test: pd.DataFrame, y_score, classes: (list, pd.
     if isinstance(y_test, pd.DataFrame):
         y_test = y_test.values
 
-    fpr = dict()
-    tpr = dict()
-    roc_auc = dict()
-    for i in class_indices:
-        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i],
-                                      sample_weight=sample_weight if sample_weight is not None else None)
-        roc_auc[i] = auc(fpr[i], tpr[i])
-
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel(),
-                                              sample_weight=sample_weight if sample_weight is not None else None)
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    fpr, roc_auc, tpr = compute_roc_auc_curve(y_test, y_score, class_indices, sample_weight)
     # Compute macro-average ROC curve and ROC area
 
     # First aggregate all false positive rates
@@ -113,6 +90,20 @@ def plot_roc_curve_multiclass(y_test: pd.DataFrame, y_score, classes: (list, pd.
     fig = configure_figure(data, title, width, height)
 
     return fig
+
+
+def compute_roc_auc_curve(y_test, y_score, class_indices, sample_weight=None):
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in class_indices:
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i],
+                                      sample_weight=sample_weight if sample_weight is not None else None)
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+    return fpr, roc_auc, tpr
 
 
 def configure_figure(data, title, width, height):
