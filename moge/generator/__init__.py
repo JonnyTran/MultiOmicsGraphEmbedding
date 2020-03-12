46d4b51934e0f46a6cca8f5a9313d69e76fbd2b5
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-03-12 14:35:15 -0500

    multiplex process_sampling_table

--- moge/generator/__init__.py
+++ moge/generator/__init__.py
@@ -1,3 +1,4 @@
 from .data_generator import DataGenerator
+from .multiplex import MultiplexGenerator
 from .sampled_generator import SampledDataGenerator
 from .subgraph_generator import SubgraphGenerator
