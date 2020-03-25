aa2e54016730046cacb8c499a2eae56911ccfcb6
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-03-25 18:31:24 -0500

    refactorings

--- moge/utils/dataframe.py
+++ moge/utils/dataframe.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
 
+def edges_dataframe(network, data=False):
+    edges_list = pd.DataFrame.from_dict(network.edges(data=data), orient='columns', )
+    edges_list.rename(columns={0: "source", 1: "target"}, inplace=True)
+    return edges_list
+
+
 def get_rename_dict(dataframe, alias_col_name):
     dataframe = dataframe[dataframe[alias_col_name].notnull()]
     b = pd.DataFrame(dataframe[alias_col_name].str.split('|').tolist(), index=dataframe.index).stack()
