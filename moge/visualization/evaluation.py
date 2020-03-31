d4629bbd417332e2f698b34ac59bdbddb1be200c
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-03-31 15:32:13 -0500

    refactorings

--- moge/visualization/evaluation.py
+++ moge/visualization/evaluation.py
@@ -12,6 +12,12 @@ from ..evaluation.classification import compute_roc_auc_curve
 def plot_roc_curve(y_test, y_score, n_classes, sample_weight=None, width=700, height=700,
                    title='Receiver operating characteristic example'):
     # Compute ROC curve and ROC area for each class
+    if isinstance(y_test, pd.DataFrame):
+        y_test = y_test.values
+
+    if isinstance(y_score, pd.DataFrame):
+        y_score = y_score.values
+
     fpr, roc_auc, tpr = compute_roc_auc_curve(y_test, y_score, range(n_classes), sample_weight)
 
     trace1 = go.Scatter(x=fpr[2], y=tpr[2],
@@ -31,7 +37,7 @@ def plot_roc_curve(y_test, y_score, n_classes, sample_weight=None, width=700, he
 def plot_roc_curve_multiclass(y_test: pd.DataFrame, y_score, classes: (list, pd.Index), sample_weight=None,
                               title='ROC Curve (multi-class)',
                               width=800, height=700):
-    if classes is not None and isinstance(y_test, pd.DataFrame):
+    if isinstance(y_test, pd.DataFrame) and classes is not None:
         class_indices = y_test.columns.get_indexer(classes)
     else:
         class_indices = range(y_test.shape[1])
@@ -65,15 +71,13 @@ def plot_roc_curve_multiclass(y_test: pd.DataFrame, y_score, classes: (list, pd.
     trace1 = go.Scatter(x=fpr["micro"], y=tpr["micro"],
                         mode='lines',
                         line=dict(color='deeppink', width=2, dash='dot'),
-                        name='micro-average ROC curve (area={0:0.2f})'
-                             ''.format(roc_auc["micro"]))
+                        name='micro-average ROC curve (area={0:0.2f})'.format(roc_auc["micro"]))
     data.append(trace1)
 
     trace2 = go.Scatter(x=fpr["macro"], y=tpr["macro"],
                         mode='lines',
                         line=dict(color='navy', width=2, dash='dot'),
-                        name='macro-average ROC curve (area={0:0.2f})'
-                             ''.format(roc_auc["macro"]))
+                        name='macro-average ROC curve (area={0:0.2f})'.format(roc_auc["macro"]))
     data.append(trace2)
     np.random.shuffle(colors)
     color_cycle = cycle(colors)
@@ -82,8 +86,7 @@ def plot_roc_curve_multiclass(y_test: pd.DataFrame, y_score, classes: (list, pd.
         trace3 = go.Scatter(x=fpr[i], y=tpr[i],
                             mode='lines',
                             line=dict(color=color, width=2),
-                            name='ROC curve of {0} (area={1:0.2f})'
-                                 ''.format(label, roc_auc[i]))
+                            name='ROC curve of {0} (area={1:0.2f})'.format(label, roc_auc[i]))
         data.append(trace3)
 
     trace4 = go.Scatter(x=[0, 1], y=[0, 1],
