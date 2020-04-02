c796c80483828a823d0506afdb916a58c28fbba1
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-04-02 17:15:51 -0500

    added embedding sequence corr comparison

--- moge/evaluation/embedding.py
+++ moge/evaluation/embedding.py
@@ -7,20 +7,27 @@ from .utils import largest_indices
 from ..network.semantic_similarity import pairwise_distances, gower_distance
 
 
-def distances_correlation(embeddings, sequences: pd.DataFrame, index: pd.Index, n_nodes=200, verbose=False):
+def distances_correlation(embeddings, labels, index: pd.Index, n_nodes=200, verbose=False):
     embedding_cov = pairwise_distances(embeddings, metric="euclidean", n_jobs=-2)
     assert embeddings.shape[0] == index.shape[0]
 
-    top_k_farthest = index[np.concatenate(largest_indices(embedding_cov, k=int(n_nodes / 4), smallest=False))]
-    top_k_closest = index[np.concatenate(largest_indices(embedding_cov, k=int(n_nodes / 4), smallest=True))]
-    nodelist = top_k_farthest.append(top_k_closest)
+    top_farthest_pairs = index[np.concatenate(largest_indices(embedding_cov, k=int(n_nodes / 4), smallest=False))]
+    top_closest_pairs = index[np.concatenate(largest_indices(embedding_cov, k=int(n_nodes / 4), smallest=True))]
+    nodelist = top_farthest_pairs.append(top_closest_pairs)
 
     embedding_distances = pairwise_distances(pd.DataFrame(embeddings, index=index).loc[nodelist],
                                              metric="euclidean", n_jobs=-2)
     embedding_distances = squareform(embedding_distances, checks=False)
 
-    seq_distances = gower_distance(sequences.loc[nodelist], verbose=verbose)
+    if isinstance(labels, pd.DataFrame):
+        label_distances = gower_distance(labels.loc[nodelist], verbose=verbose)
+    else:
+        label_distances = pairwise_distances(pd.DataFrame(labels, index=index).loc[nodelist], metric="cosine",
+                                             n_jobs=-2)
+        label_distances = squareform(label_distances, checks=False)
 
-    print(f"embedding dists {embedding_distances.shape}, seq dists {seq_distances.shape}") if verbose else None
-    r, p_val = pearsonr(x=embedding_distances[~np.isnan(seq_distances)], y=seq_distances[~np.isnan(seq_distances)])
+    print(f"embedding dists {embedding_distances.shape}, seq dists {label_distances.shape}") if verbose else None
+
+    r, p_val = pearsonr(x=embedding_distances[~np.isnan(label_distances)],
+                        y=label_distances[~np.isnan(label_distances)])
     return r, p_val
