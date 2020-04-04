57552751129448655afe4f29a187e0484deff90e
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-04-04 13:28:29 -0500

    annotations revert fixes

--- moge/evaluation/embedding.py
+++ moge/evaluation/embedding.py
@@ -10,6 +10,7 @@ from ..network.semantic_similarity import pairwise_distances, gower_distance
 def distances_correlation(embeddings, labels, index: pd.Index, n_nodes=200, verbose=False):
     embedding_cov = pairwise_distances(embeddings, metric="euclidean", n_jobs=-2)
     assert embeddings.shape[0] == index.shape[0]
+    assert labels.shape[0] == index.shape[0]
 
     top_farthest_pairs = index[np.concatenate(largest_indices(embedding_cov, k=int(n_nodes / 4), smallest=False))]
     top_closest_pairs = index[np.concatenate(largest_indices(embedding_cov, k=int(n_nodes / 4), smallest=True))]
@@ -18,6 +19,7 @@ def distances_correlation(embeddings, labels, index: pd.Index, n_nodes=200, verb
     embedding_distances = pairwise_distances(pd.DataFrame(embeddings, index=index).loc[nodelist],
                                              metric="euclidean", n_jobs=-2)
     embedding_distances = squareform(embedding_distances, checks=False)
+    print("embedding_distances", embedding_distances) if verbose else None
 
     if isinstance(labels, pd.DataFrame):
         label_distances = gower_distance(labels.loc[nodelist], verbose=verbose)
@@ -25,6 +27,9 @@ def distances_correlation(embeddings, labels, index: pd.Index, n_nodes=200, verb
         label_distances = pairwise_distances(pd.DataFrame(labels, index=index).loc[nodelist], metric="cosine",
                                              n_jobs=-2)
         label_distances = squareform(label_distances, checks=False)
+    print("label_distances", label_distances) if verbose else None
+
+    assert label_distances.shape[0] == embedding_distances.shape[0]
 
     print(f"embedding dists {embedding_distances.shape}, seq dists {label_distances.shape}") if verbose else None
 
