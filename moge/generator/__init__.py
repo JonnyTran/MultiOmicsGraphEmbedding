0d84d1f4d7bd17680e95a68f3bd37a62cba0eb3b
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-04-16 15:47:44 -0500

    encoder-embedder-classifier implemented

--- moge/generator/__init__.py
+++ moge/generator/__init__.py
@@ -1,4 +1,4 @@
 from .data_generator import DataGenerator
-from .dataset import SubgraphDataset
 from .sampled_generator import SampledDataGenerator
 from .subgraph_generator import SubgraphGenerator
+from .dataset import SubgraphDataset
