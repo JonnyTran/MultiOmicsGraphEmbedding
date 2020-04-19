48f0c5f89cf979bc81b7c633f2a9291fa3c9ed7f
JonnyTran <jonnytran@github.com>
2020-04-19 15:16:53 -0500

    latest

--- moge/generator/__init__.py
+++ moge/generator/__init__.py
@@ -1,5 +1,4 @@
 from .data_generator import DataGenerator
-from .dataset import SubgraphDataset, GraphSampler
-from .dataset import SubgraphDataset, GraphSampler
 from .sampled_generator import SampledDataGenerator
 from .subgraph_generator import SubgraphGenerator
+from .dataset import SubgraphDataset, GraphSampler
