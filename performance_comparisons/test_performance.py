46f5dd92fa2644c12abc47664d8971e0688a6410
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-06-25 23:23:25 -0500

    added embedding indexing feature

--- performance_comparisons/test_performance.py
+++ performance_comparisons/test_performance.py
@@ -23,7 +23,7 @@ from pytorch_lightning.loggers import WandbLogger
 def train(hparams):
     EMBEDDING_DIM = 128
     NUM_GPUS = 1
-    METRICS = ["accuracy", "precision", "recall"]
+    METRICS = ["accuracy", "precision", "recall", "top_k"]
 
     if hparams.dataset == "ACM":
         dataset = HeterogeneousNetworkDataset(ACM_HANDataset(),
@@ -40,8 +40,9 @@ def train(hparams):
                                               node_types=["M"], metapath=["MAM", "MDM", "MYM"],
                                               train_ratio=hparams.train_ratio)
     elif hparams.dataset == "AMiner":
-        dataset = HeterogeneousNetworkDataset(AMiner("datasets/aminer"),
-                                              node_types=None, head_node_type="author",
+        dataset = HeterogeneousNetworkDataset(AMiner("datasets/aminer"), node_types=None, head_node_type="author",
+                                              metapath=[('paper', 'written by', 'author'),
+                                                        ('venue', 'published', 'paper')],
                                               train_ratio=hparams.train_ratio)
     elif hparams.dataset == "BlogCatalog":
         dataset = HeterogeneousNetworkDataset("/home/jonny/Downloads/blogcatalog6k.mat",
@@ -64,7 +65,7 @@ def train(hparams):
     elif hparams.method == "GTN":
         USE_AMP = True
         model_hparams = {
-            "embedding_dim": EMBEDDING_DIM,
+            "embedding_dim": int(EMBEDDING_DIM / 2),
             "num_channels": 1,
             "batch_size": 128 * NUM_GPUS,
             "train_ratio": dataset.train_ratio,
@@ -98,12 +99,13 @@ def train(hparams):
     trainer = Trainer(
         gpus=NUM_GPUS,
         distributed_backend='dp' if NUM_GPUS > 1 else None,
-        #     auto_lr_find=True,
+        # auto_lr_find=True,
         max_epochs=MAX_EPOCHS,
-        callbacks=[EarlyStopping(monitor='loss', patience=1, min_delta=0.0001),
-                   EarlyStopping(monitor='val_loss', patience=2, min_delta=0.0001), ],
+        early_stop_callback=EarlyStopping(monitor='val_loss', patience=2, min_delta=0.01),
+        # callbacks=[EarlyStopping(monitor='loss', patience=1, min_delta=0.0001),
+        #            EarlyStopping(monitor='val_loss', patience=2, min_delta=0.0001), ],
         logger=wandb_logger,
-        #     regularizers=regularizers,
+        # regularizers=regularizers,
         weights_summary='top',
         use_amp=USE_AMP,
         amp_level='O1' if USE_AMP else None,
