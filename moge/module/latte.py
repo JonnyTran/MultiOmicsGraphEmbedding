2e92082f3904fc56525e52d4543bb1b57c39f497
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-08-01 23:36:44 -0500

    refactored datasets

--- moge/module/latte.py
+++ moge/module/latte.py
@@ -300,13 +300,15 @@ class LATTELayer(MessagePassing, pl.LightningModule):
                     edge_index = edge_index_dict[metapath]
                 if edge_index.size(1) <= 5: continue
 
+                print("\n", metapath, num_node_head, num_node_tail)
+                print("edge_index", edge_index.size())
+                print("alpha_r[metapath]", alpha_r[metapath].size())
+                print("alpha_l[metapath]", alpha_l[metapath].size())
                 emb_relation_agg[head_type][:, i] = self.propagate(
                     edge_index,
                     size=(num_node_tail, num_node_head),
                     x=(h_dict[tail_type], h_dict[head_type]),
                     alpha=(alpha_r[metapath], alpha_l[metapath]))
-                # print(" alpha_r[metapath]", alpha_r[metapath][:5])
-                # print("alpha_l[metapath]", alpha_l[metapath][:5])
 
             emb_relation_agg[node_type][:, -1] = h_dict[node_type]
             emb_output[node_type] = torch.matmul(emb_relation_agg[node_type].permute(0, 2, 1),
@@ -325,7 +327,7 @@ class LATTELayer(MessagePassing, pl.LightningModule):
 
     def message(self, x_j, alpha_j, alpha_i, index, ptr, size_i):
         # print("alpha_i", alpha_i[:5])
-        # print("alpha_j", alpha_j[:5])
+        print("alpha_j", alpha_j.size())
         alpha = alpha_j if alpha_i is None else alpha_j + alpha_i
         # alpha = F.leaky_relu(alpha, 0.2)
         alpha = softmax(alpha, index=index, ptr=ptr, num_nodes=size_i)
