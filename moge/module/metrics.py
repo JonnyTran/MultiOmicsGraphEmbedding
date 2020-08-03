018b5a16d8c48f420597f16e16f163eae67f66a9
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-08-03 18:33:02 -0500

    refactored datasets

--- moge/module/metrics.py
+++ moge/module/metrics.py
@@ -131,8 +131,10 @@ class OGBEvaluator(Metric):
         self.y_pred.append(y_pred)
 
     def compute(self, prefix=None):
-        print("y_pred", self.y_pred[-1].shape, pd.Series(self.y_pred[-1].detach().cpu().numpy()).value_counts())
-        print("y_true", self.y_true[-1].shape, pd.Series(self.y_true[-1].detach().cpu().numpy()).value_counts())
+        print("y_pred", self.y_pred[-1].shape,
+              pd.Series(self.y_pred[-1].squeeze(-1).detach().cpu().numpy()).value_counts())
+        print("y_true", self.y_true[-1].shape,
+              pd.Series(self.y_true[-1].squeeze(-1).detach().cpu().numpy()).value_counts())
         if isinstance(self.evaluator, NodeEvaluator):
             output = self.evaluator.eval({"y_pred": torch.cat(self.y_pred, dim=0),
                                           "y_true": torch.cat(self.y_true, dim=0)})
