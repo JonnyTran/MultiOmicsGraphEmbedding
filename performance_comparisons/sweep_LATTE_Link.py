62c4c597557bc550dd128119c0ac79905a7e9e95
JonnyTran <jonnytran@github.com>
2020-08-09 15:22:13 -0500

    loss working

--- performance_comparisons/sweep_LATTE_Link.py
+++ performance_comparisons/sweep_LATTE_Link.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     parser.add_argument('--dataset', type=str, default="ogbl-biokg")
     parser.add_argument('--embedding_dim', type=int, default=128)
     parser.add_argument('--t_order', type=int, default=2)
-    parser.add_argument('--batch_size', type=int, default=2097152)
+    parser.add_argument('--batch_size', type=int, default=1048576)
     parser.add_argument('--n_neighbors_1', type=int, default=30)
     parser.add_argument('--activation', type=str, default="tanh")
     parser.add_argument('--attn_activation', type=str, default="LeakyReLU")
@@ -76,6 +76,7 @@ if __name__ == "__main__":
 
     parser.add_argument('--nb_cls_dense_size', type=int, default=0)
     parser.add_argument('--nb_cls_dropout', type=float, default=0.2)
+
     parser.add_argument('--use_proximity_loss', type=bool, default=True)
     parser.add_argument('--neg_sampling_ratio', type=float, default=2.0)
     parser.add_argument('--use_class_weights', type=bool, default=False)
