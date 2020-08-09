06491cf3672504f3b806a3e1b2c3f94002caedc9
JonnyTran <jonnytran@github.com>
2020-08-09 15:24:56 -0500

    loss working

--- performance_comparisons/sweep_LATTE_Link.py
+++ performance_comparisons/sweep_LATTE_Link.py
@@ -67,7 +67,7 @@ if __name__ == "__main__":
     # parametrize the network
     parser.add_argument('--dataset', type=str, default="ogbl-biokg")
     parser.add_argument('--embedding_dim', type=int, default=128)
-    parser.add_argument('--t_order', type=int, default=2)
+    parser.add_argument('--t_order', type=int, default=1)
     parser.add_argument('--batch_size', type=int, default=1048576)
     parser.add_argument('--n_neighbors_1', type=int, default=30)
     parser.add_argument('--activation', type=str, default="tanh")
