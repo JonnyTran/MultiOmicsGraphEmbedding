6dbf4a72f9283ab4f5d030983581d661bbdd6274
Jonny Tran <nhat.tran@mavs.uta.edu>
2020-08-10 13:04:14 -0500

    working link_sampler.py

--- moge/methods/node_classification.py
+++ moge/methods/node_classification.py
@@ -29,6 +29,7 @@ class NodeClfMetrics(pl.LightningModule):
                                      multilabel=dataset.multilabel, metrics=metrics)
         self.test_metrics = Metrics(prefix="test_", loss_type=hparams.loss_type, n_classes=dataset.n_classes,
                                     multilabel=dataset.multilabel, metrics=metrics)
+        hparams.name = self.name()
         self.hparams = hparams
 
     def name(self):
