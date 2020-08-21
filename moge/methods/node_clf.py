9fb86310a256d9d24757cdada66aaf34e43a6d36
JonnyTran <JonnyTran@github.com>
2020-08-20 21:53:28 -0500

    added edge_sampler.py

--- moge/methods/node_clf.py
+++ moge/methods/node_clf.py
@@ -52,7 +52,7 @@ class NodeClfMetrics(pl.LightningModule):
         avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean().item()
         logs = self.valid_metrics.compute_metrics()
         logs = _fix_dp_return_type(logs, device=outputs[0]["val_loss"].device)
-        print({k: np.around(v.item(), decimals=3) for k, v in logs.items()})
+        # print({k: np.around(v.item(), decimals=3) for k, v in logs.items()})
 
         logs.update({"val_loss": avg_loss})
         self.valid_metrics.reset_metrics()
