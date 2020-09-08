6a21dcc9b505c9ae8f9d7bb6b0383e533866f314
JonnyTran <JonnyTran@github.com>
2020-09-07 22:47:38 -0500

    fixes to sample()

--- run/utils.py
+++ run/utils.py
@@ -24,25 +24,25 @@ def load_node_dataset(dataset, method, hparams, train_ratio=None, dir_path="~/Bi
 
     elif dataset == "ACM":
         if method == "HAN" or method == "MetaPath2Vec":
-            dataset = HeteroNeighborSampler(ACM_HANDataset(), [25, 10], node_types=["P"],
+            dataset = HeteroNeighborSampler(ACM_HANDataset(), [25, 20], node_types=["P"],
                                             metapaths=["PAP", "PSP"] if "LATTE" in method else None,
                                             add_reverse_metapaths=True,
                                             head_node_type="P", resample_train=train_ratio, inductive=hparams.inductive)
         else:
-            dataset = HeteroNeighborSampler(ACM_GTNDataset(), [25, 10], node_types=["P"],
+            dataset = HeteroNeighborSampler(ACM_GTNDataset(), [25, 20], node_types=["P"],
                                             metapaths=["PAP", "PA_P", "PSP", "PS_P"] if "LATTE" in method else None,
                                             add_reverse_metapaths=False,
                                             head_node_type="P", resample_train=train_ratio, inductive=hparams.inductive)
 
     elif dataset == "DBLP":
         if method == "HAN" or "LATTE" in method:
-            dataset = HeteroNeighborSampler(DBLP_HANDataset(), [25, 10], node_types=["A"],
+            dataset = HeteroNeighborSampler(DBLP_HANDataset(), [25, 20], node_types=["A"],
                                             metapaths=["ACA", "APA", "ATA"] if "LATTE" in method else None,
                                             head_node_type="A",
                                             add_reverse_metapaths=True,
                                             resample_train=train_ratio, inductive=hparams.inductive)
         else:
-            dataset = HeteroNeighborSampler(DBLP_GTNDataset(), [25, 10], node_types=["A"],
+            dataset = HeteroNeighborSampler(DBLP_GTNDataset(), [25, 20], node_types=["A"],
                                             metapaths=["APA", "AP_A", "ACA", "AC_A"] if "LATTE" in method else None,
                                             head_node_type="A",
                                             add_reverse_metapaths=False,
@@ -50,23 +50,23 @@ def load_node_dataset(dataset, method, hparams, train_ratio=None, dir_path="~/Bi
 
     elif dataset == "IMDB":
         if method == "HAN" or method == "MetaPath2Vec":
-            dataset = HeteroNeighborSampler(IMDB_HANDataset(), [25, 10], node_types=["M"],
+            dataset = HeteroNeighborSampler(IMDB_HANDataset(), [25, 20], node_types=["M"],
                                             metapaths=["MAM", "MDM", "MWM"] if "LATTE" in method else None,
                                             add_reverse_metapaths=True,
                                             head_node_type="M",
                                             resample_train=train_ratio, inductive=hparams.inductive)
         else:
-            dataset = HeteroNeighborSampler(IMDB_GTNDataset(), neighbor_sizes=[25, 10], node_types=["M"],
+            dataset = HeteroNeighborSampler(IMDB_GTNDataset(), neighbor_sizes=[25, 20], node_types=["M"],
                                             metapaths=["MDM", "MD_M", "MAM", "MA_M"] if "LATTE" in method else None,
                                             add_reverse_metapaths=False,
                                             head_node_type="M", inductive=hparams.inductive)
     elif dataset == "AMiner":
-        dataset = HeteroNeighborSampler(AMiner("datasets/aminer"), [25, 10], node_types=None,
+        dataset = HeteroNeighborSampler(AMiner("datasets/aminer"), [25, 20], node_types=None,
                                         metapaths=[('paper', 'written by', 'author'),
                                                    ('venue', 'published', 'paper')], head_node_type="author",
                                         resample_train=train_ratio, inductive=hparams.inductive)
     elif dataset == "BlogCatalog":
-        dataset = HeteroNeighborSampler("datasets/blogcatalog6k.mat", [25, 10], node_types=["user", "tag"],
+        dataset = HeteroNeighborSampler("datasets/blogcatalog6k.mat", [25, 20], node_types=["user", "tag"],
                                         head_node_type="user", resample_train=train_ratio, inductive=hparams.inductive)
     else:
         raise Exception(f"dataset {dataset} not found")
