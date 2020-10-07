747712c9d375cf3c382bb7371e45e2a304ff54e5
JonnyTran <JonnyTran@github.com>
2020-10-07 17:50:53 -0500

    refactors

--- moge/generator/__init__.py
+++ moge/generator/__init__.py
@@ -1,6 +1,6 @@
+from .network import HeteroNetDataset
 from .PyG.edge_sampler import EdgeSampler
 from .PyG.node_sampler import HeteroNeighborSampler
 from .PyG.triplet_sampler import TripletSampler
 from .dgl.node_sampler import DGLNodeSampler
-from .network import HeteroNetDataset
 from .nx.data_generator import DataGenerator, GeneratorDataset
