40a33470ffc9d56ffc286cf84eff0a355607d510
Jonny Tran <nhat.tran@mavs.uta.edu>
2021-02-20 14:46:48 -0600

    latest

--- moge/generator/PyG/edge_sampler.py
+++ moge/generator/PyG/edge_sampler.py
@@ -12,9 +12,9 @@ from moge.module.PyG.latte import tag_negative, is_negative
 class EdgeSampler(HeteroNetDataset):
     def __init__(self, dataset, node_types=None, metapaths=None, head_node_type=None, directed=True,
                  resample_train=None,
-                 add_reverse_metapaths=True):
+                 add_reverse_metapaths=True, **kwargs):
         super(EdgeSampler, self).__init__(dataset, node_types, metapaths, head_node_type, directed, resample_train,
-                                          add_reverse_metapaths)
+                                          add_reverse_metapaths, **kwargs)
 
     def process_PygLinkDataset_homo(self, dataset: PygLinkPropPredDataset):
         data = dataset[0]
