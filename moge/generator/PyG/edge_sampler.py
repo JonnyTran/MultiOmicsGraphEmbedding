813e3b5f6f3d7e9b33ec80c0931ba4dd1bceb29d
Jonny Tran <nhat.tran@mavs.uta.edu>
2021-02-20 21:37:25 -0600

    latest

--- moge/generator/PyG/edge_sampler.py
+++ moge/generator/PyG/edge_sampler.py
@@ -45,9 +45,14 @@ class EdgeSampler(HeteroNetDataset):
         # Concat pos edges
         for key in train_triples.keys():
             if isinstance(train_triples[key], torch.Tensor):
-                self.triples[EdgeSampler.DEFAULT_METAPATH] = torch.cat(
-                    [valid_triples[key], test_triples[key], train_triples[key]],
-                    dim=0).permute(1, 0)
+                if valid_triples[key].dim() == 2:
+                    self.triples[EdgeSampler.DEFAULT_METAPATH] = torch.cat(
+                        [valid_triples[key], test_triples[key], train_triples[key]],
+                        dim=0).permute(1, 0)
+                else:
+                    self.triples[EdgeSampler.DEFAULT_METAPATH] = torch.cat(
+                        [valid_triples[key], test_triples[key], train_triples[key]],
+                        dim=0)
             else:
                 self.triples[EdgeSampler.DEFAULT_METAPATH] = np.array(
                     valid_triples[key] + test_triples[key] + train_triples[key])
