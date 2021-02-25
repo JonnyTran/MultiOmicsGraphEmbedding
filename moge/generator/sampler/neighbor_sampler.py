db1a78709178589b01e93257ef0dcc7774496751
JonnyTran <JonnyTran@github.com>
2021-02-25 12:33:22 -0600

    only selects largest sampled adjs

--- moge/generator/sampler/neighbor_sampler.py
+++ moge/generator/sampler/neighbor_sampler.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 from abc import ABCMeta, abstractmethod
 from collections import OrderedDict
+from typing import Union, List, Optional
 
 import numpy as np
 import torch
 import torch_geometric
+from torch import Tensor
+from torch_geometric.data.sampler import Adj, EdgeIndex
 from torch_geometric.utils.hetero import group_hetero_graph
-from torch_sparse import coalesce
+from torch_sparse import coalesce, SparseTensor
 
 
 class Sampler(metaclass=ABCMeta):
@@ -21,6 +24,38 @@ class Sampler(metaclass=ABCMeta):
     @abstractmethod
     def get_nodes_dict(self, adjs, n_id):
         pass
+
+
+class HeteroNeighborSampler(torch_geometric.data.NeighborSampler):
+    def __init__(self, edge_index: Union[Tensor, SparseTensor], sizes: List[int], node_idx: Optional[Tensor] = None,
+                 num_nodes: Optional[int] = None, return_e_id: bool = True, **kwargs):
+        super().__init__(edge_index, sizes, node_idx, num_nodes, return_e_id, **kwargs)
+
+        self.adj_t.storage.colptr()
+
+    def sample(self, batch):
+        if not isinstance(batch, Tensor):
+            batch = torch.tensor(batch)
+
+        batch_size: int = len(batch)
+
+        adjs = []
+        n_id = batch
+        for size in self.sizes:
+            adj_t, n_id = self.adj_t.sample_adj(n_id, size, replace=False)
+            e_id = adj_t.storage.value()
+            size = adj_t.sparse_sizes()[::-1]
+            if self.__val__ is not None:
+                adj_t.set_value_(self.__val__[e_id], layout='coo')
+
+            if self.is_sparse_tensor:
+                adjs.append(Adj(adj_t, e_id, size))
+            else:
+                row, col, _ = adj_t.coo()
+                edge_index = torch.stack([row, col], dim=0)
+                adjs.append(EdgeIndex(edge_index, e_id, size))
+
+        return batch_size, n_id, adjs
 
 
 class NeighborSampler(Sampler):
@@ -39,9 +74,9 @@ class NeighborSampler(Sampler):
         self.int2edge_type = {type_int: edge_type for edge_type, type_int in self.key2int.items() if
                               edge_type in edge_index_dict}
 
-        self.neighbor_sampler = torch_geometric.data.NeighborSampler(self.edge_index, node_idx=None,
-                                                                     sizes=neighbor_sizes, batch_size=128,
-                                                                     shuffle=True)
+        self.neighbor_sampler = HeteroNeighborSampler(self.edge_index, node_idx=None,
+                                                      sizes=neighbor_sizes, batch_size=128,
+                                                      shuffle=True)
 
     def sample(self, node_ids: dict):
         local_node_idx = self.get_global_nidx(node_ids)
@@ -90,52 +125,53 @@ class NeighborSampler(Sampler):
                                 range(len(sampled_local_nodes[node_type])))
                             ) for node_type in sampled_local_nodes}
 
+        adj = adjs[-1]
+
         edge_index_dict = {}
-        for adj in adjs:
-            for edge_type_id in self.edge_type[adj.e_id].unique():
-                metapath = self.int2edge_type[edge_type_id.item()]
-                head_type, tail_type = metapath[0], metapath[-1]
+        for edge_type_id in self.edge_type[adj.e_id].unique():
+            metapath = self.int2edge_type[edge_type_id.item()]
+            head_type, tail_type = metapath[0], metapath[-1]
 
-                # Filter edges to correct edge_type_id
-                edge_mask = self.edge_type[adj.e_id] == edge_type_id
-                edge_index = adj.edge_index[:, edge_mask]
+            # Filter edges to correct edge_type_id
+            edge_mask = self.edge_type[adj.e_id] == edge_type_id
+            edge_index = adj.edge_index[:, edge_mask]
 
-                # convert from "sampled_edge_index" to global index
-                edge_index[0] = n_id[edge_index[0]]
-                edge_index[1] = n_id[edge_index[1]]
+            # convert from "sampled_edge_index" to global index
+            edge_index[0] = n_id[edge_index[0]]
+            edge_index[1] = n_id[edge_index[1]]
 
-                # Filter nodes for only head node type
-                if filter_nodes is True:
-                    # If node_type==self.head_node_type, then remove edge_index with nodes not in allowed_nodes_idx
-                    allowed_nodes_idx = self.local2global[self.head_node_type][sampled_local_nodes[self.head_node_type]]
+            # Filter nodes for only head node type
+            if filter_nodes is True:
+                allowed_nodes_idx = self.local2global[self.head_node_type][sampled_local_nodes[self.head_node_type]]
 
-                    if head_type == self.head_node_type and tail_type == self.head_node_type:
-                        mask = np.isin(edge_index[0], allowed_nodes_idx) & np.isin(edge_index[1], allowed_nodes_idx)
-                        edge_index = edge_index[:, mask]
-                    elif head_type == self.head_node_type:
-                        mask = np.isin(edge_index[0], allowed_nodes_idx)
-                        edge_index = edge_index[:, mask]
-                    elif tail_type == self.head_node_type:
-                        mask = np.isin(edge_index[1], allowed_nodes_idx)
-                        edge_index = edge_index[:, mask]
-
-                # Filter nodes from all node types
-                elif filter_nodes == 2:
-                    if head_type not in relabel_nodes or tail_type not in relabel_nodes: continue
-
-                    allowed_nodes_idx = torch.cat([self.local2global[ntype][n_ids] \
-                                                   for ntype, n_ids in sampled_local_nodes.items()], dim=0)
-
-                    mask = np.isin(edge_index[0], allowed_nodes_idx) & np.isin(edge_index[1], allowed_nodes_idx)
+                # If node_type==self.head_node_type, then remove edge_index with any nodes not in allowed_nodes_idx
+                if head_type == self.head_node_type and tail_type == self.head_node_type:
+                    mask = np.isin(edge_index, allowed_nodes_idx, assume_unique=True).all(0)
+                    edge_index = edge_index[:, mask]
+                elif head_type == self.head_node_type:
+                    mask = np.isin(edge_index[0], allowed_nodes_idx, assume_unique=True)
+                    edge_index = edge_index[:, mask]
+                elif tail_type == self.head_node_type:
+                    mask = np.isin(edge_index[1], allowed_nodes_idx, assume_unique=True)
                     edge_index = edge_index[:, mask]
 
-                if edge_index.shape[1] == 0: continue
+            # Filter nodes from all node types
+            elif filter_nodes == 2:
+                if head_type not in relabel_nodes or tail_type not in relabel_nodes: continue
 
-                # Convert node global index -> local index -> batch index
-                edge_index[0] = self.local_node_idx[edge_index[0]].apply_(relabel_nodes[head_type].get)
-                edge_index[1] = self.local_node_idx[edge_index[1]].apply_(relabel_nodes[tail_type].get)
+                allowed_nodes_idx = torch.cat([self.local2global[ntype][n_ids] \
+                                               for ntype, n_ids in sampled_local_nodes.items()], dim=0)
 
-                edge_index_dict.setdefault(metapath, []).append(edge_index)
+                mask = np.isin(edge_index, allowed_nodes_idx, assume_unique=True).all(0)
+                edge_index = edge_index[:, mask]
+
+            if edge_index.shape[1] == 0: continue
+
+            # Convert node global index -> local index -> batch index
+            edge_index[0] = self.local_node_idx[edge_index[0]].apply_(relabel_nodes[head_type].get)
+            edge_index[1] = self.local_node_idx[edge_index[1]].apply_(relabel_nodes[tail_type].get)
+
+            edge_index_dict.setdefault(metapath, []).append(edge_index)
 
         # Join edges from the adjs (from iterative layer-wise sampling)
         edge_index_dict = {metapath: torch.cat(e_index_list, dim=1) \
