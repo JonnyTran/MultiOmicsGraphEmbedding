97f8fd061f367affe553c29a06402df5b394adb7
JonnyTran <JonnyTran@github.com>
2021-06-19 12:15:05 -0500

    working

--- moge/module/PyG/node_clf.py
+++ moge/module/PyG/node_clf.py
@@ -117,7 +117,7 @@ class LATTENodeClf(NodeClfTrainer):
         if self.hparams.use_proximity:
             val_loss = val_loss + proximity_loss
 
-        self.log("val_loss", val_loss, on_step=True)
+        self.log("val_loss", val_loss)
 
         return val_loss
 
@@ -136,7 +136,7 @@ class LATTENodeClf(NodeClfTrainer):
         if self.hparams.use_proximity:
             test_loss = test_loss + proximity_loss
 
-        self.log("test_loss", test_loss, on_step=True)
+        self.log("test_loss", test_loss)
 
         return test_loss
 
