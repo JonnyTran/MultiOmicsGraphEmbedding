caab31d24752272e28c7fbb2c62e6f3537cb9d1e
JonnyTran <JonnyTran@github.com>
2021-06-23 23:11:39 -0500

    performance enhancements

--- moge/module/PyG/node_clf.py
+++ moge/module/PyG/node_clf.py
@@ -30,7 +30,7 @@ class LATTENodeClf(NodeClfTrainer):
         self.dataset = dataset
         self.multilabel = dataset.multilabel
         self.y_types = list(dataset.y_dict.keys())
-        self._name = f"LATTE-{hparams.n_layers}{' proximity' if hparams.use_proximity else ''}"
+        self._name = f"LATTE-{hparams.n_layers} ({hparams.t_order}-order){' proximity' if hparams.use_proximity else ''}"
         self.collate_fn = collate_fn
 
         self.embedder = LATTE(n_layers=hparams.n_layers,
