8567fd7fc90a0c3fd06e4d833fca365b13a18de0
JonnyTran <JonnyTran@github.com>
2021-06-26 00:54:20 -0500

    added datashade heatmap

--- moge/visualization/data.py
+++ moge/visualization/data.py
@@ -7,6 +7,26 @@ from scipy.sparse import coo_matrix
 from scipy.sparse import issparse
 from sklearn.metrics import classification_report
 
+import plotly.express as px
+import datashader as ds
+from datashader import reductions as rd
+import xarray as xr
+
+
+def heatmap_fast(mtx, x_label, y_label, size=1000):
+    pw_s = xr.DataArray(mtx, coords=[(x_label, np.arange(mtx.shape[0])),
+                                     (y_label, np.arange(mtx.shape[1]))])
+    cvs = ds.Canvas(plot_width=int(size * mtx.shape[0] / sum(mtx.shape)),
+                    plot_height=int(size * mtx.shape[1] / sum(mtx.shape)),
+                    x_range=(0, mtx.shape[1]),
+                    y_range=(0, mtx.shape[0]))
+
+    agg = cvs.raster(pw_s, agg=rd.max())
+
+    fig = px.imshow(agg)
+    fig['layout'].update(autosize=False)
+    return fig
+
 
 def clf_report(y_true, y_pred, classes, threshold=0.5, top_k=20):
     results = pd.DataFrame(classification_report(y_true=y_true,
