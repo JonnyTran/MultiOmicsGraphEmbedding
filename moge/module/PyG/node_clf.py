1b07347cb206d2774120e04a89b9862c6ee2eae2
JonnyTran <JonnyTran@github.com>
2021-06-27 21:49:40 -0500

    able to run wandb sweeps

--- moge/module/PyG/node_clf.py
+++ moge/module/PyG/node_clf.py
@@ -115,7 +115,7 @@ class LATTENodeClf(NodeClfTrainer):
 
         self.train_metrics.update_metrics(y_pred, y_true, weights=weights)
 
-        if batch_nb % 100 == 0:
+        if batch_nb % 250 == 0:
             logs = self.train_metrics.compute_metrics()
             self.log("loss", loss, logger=True, on_step=True)
         else:
