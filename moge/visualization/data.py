3c7e153c1f3f0138843475fa0471ea1fc8421095
JonnyTran <JonnyTran@github.com>
2021-07-03 15:30:54 -0500

    model activation changes

--- moge/visualization/data.py
+++ moge/visualization/data.py
@@ -31,6 +31,24 @@ def rasterize_matrix(mtx: np.array, x_label="X", y_label="Y", size=1000):
     fig = hv.render(rasterized_img, backend="plotly")
     return go.Figure(fig)
 
+
+def heatmap_fast(mtx, x_label, y_label, size=1000):
+    pw_s = xr.DataArray(mtx,
+                        # coords=[(x_label, np.arange(mtx.shape[0])),
+                        #              (y_label, np.arange(mtx.shape[1]))]
+                        )
+    cvs = ds.Canvas(plot_width=int(size * mtx.shape[0] / sum(mtx.shape)),
+                    plot_height=int(size * mtx.shape[1] / sum(mtx.shape)),
+                    x_range=(0, mtx.shape[1]),
+                    y_range=(0, mtx.shape[0]))
+
+    agg = cvs.raster(pw_s, agg=rd.mean())
+
+    fig = px.imshow(agg, labels={"x": x_label, "y": y_label})
+    fig['layout'].update(autosize=False)
+    return fig
+
+
 def clf_report(y_true, y_pred, classes, threshold=0.5, top_k=20):
     results = pd.DataFrame(classification_report(y_true=y_true,
                                                  y_pred=(y_pred >= threshold),
