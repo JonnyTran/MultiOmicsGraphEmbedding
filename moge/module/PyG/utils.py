0bbddb0cf76d8a70754ecb4992dcef5e5bfa2831
JonnyTran <JonnyTran@github.com>
2021-07-05 23:26:11 -0500

    latest

--- moge/module/PyG/utils.py
+++ moge/module/PyG/utils.py
@@ -51,9 +51,9 @@ def filter_metapaths(metapaths: List[Tuple[str]],
     def filter_func(metapath: Tuple[str]):
         condition = True
 
-        if order and isinstance(order, int):
+        if order is not None and isinstance(order, int):
             condition = condition & (len(metapath[1::2]) == order)
-        elif order and isinstance(order, Iterable):
+        elif order is not None and isinstance(order, Iterable):
             condition = condition & (len(metapath[1::2]) in order)
 
         if head_type:
@@ -81,12 +81,13 @@ def get_edge_index_values(edge_index_tup: Union[Tuple[torch.Tensor, torch.Tensor
 
     elif isinstance(edge_index_tup, torch.Tensor) and edge_index_tup.size(1) > 0:
         edge_index = edge_index_tup
-        edge_values = torch.ones(edge_index_tup.size(1), dtype=torch.float64, device=edge_index_tup.device)
+        edge_values = None
+        # edge_values = torch.ones(edge_index_tup.size(1), dtype=torch.float, device=edge_index_tup.device)
     else:
         return None, None
 
-    if edge_values.dtype != torch.float:
-        edge_values = edge_values.to(torch.float)
+    # if edge_values.dtype != torch.float:
+    #     edge_values = edge_values.to(torch.float)
 
     return edge_index, edge_values
 
@@ -107,7 +108,7 @@ def join_edge_indexes(edge_index_dict_A: Dict[Tuple, Tuple[torch.Tensor]],
 
     for metapath_b, edge_index_b in edge_index_dict_B.items():
         edge_index_b, values_b = get_edge_index_values(edge_index_b, filter_edge=False)
-        if edge_index_b is None or edge_index_b.size(1) < 5: continue
+        if edge_index_b is None or edge_index_b.size(1) < 1: continue
 
         # In the current LATTE layer that calls this method, a metapath is not higher-order
         if metapaths and metapath_b in metapaths:
@@ -122,23 +123,23 @@ def join_edge_indexes(edge_index_dict_A: Dict[Tuple, Tuple[torch.Tensor]],
             edge_index_a, values_a = get_edge_index_values(edge_index_a,
                                                            filter_edge=True if edge_threshold else False,
                                                            threshold=edge_threshold)
-            if edge_index_a is None or edge_index_a.size(1) < 5 or is_negative(metapath_a): continue
+            if edge_index_a is None or edge_index_a.size(1) < 1 or is_negative(metapath_a): continue
 
             head, middle, tail = metapath_a[0], metapath_a[-1], metapath_b[-1]
             a_order = len(metapath_a[1::2])
-            m = sizes[layer - a_order][head][0]
-            k = sizes[layer - a_order][middle][1]
-            n = sizes[layer][tail][1]
+            m = sizes[layer - a_order][head][0].item()
+            k = sizes[layer - a_order][middle][1].item()
+            n = sizes[layer][tail][1].item()
 
             try:
                 if values_a.dim() > 1 and values_a.size(1) > 1:
                     new_values = []
                     for d in range(values_a.size(1)):
-                        new_edge_index, values = adamic_adar(indexA=edge_index_a, valueA=values_a[:, d],
-                                                             indexB=edge_index_b, valueB=values_b[:, d],
-                                                             m=m, k=k, n=n,
-                                                             # sampling=edge_sampling,
-                                                             coalesced=True)
+                        new_edge_index, values = spspmm(indexA=edge_index_a, valueA=values_a[:, d],
+                                                        indexB=edge_index_b, valueB=values_b[:, d],
+                                                        m=m, k=k, n=n,
+                                                        # sampling=edge_sampling,
+                                                        coalesced=True)
                         new_values.append(values)
 
                     new_values = torch.stack(new_values, dim=1)
@@ -149,11 +150,11 @@ def join_edge_indexes(edge_index_dict_A: Dict[Tuple, Tuple[torch.Tensor]],
                     if values_b.dim() > 1 and values_b.size(1) == 1:
                         values_b = values_b.squeeze(-1)
 
-                    new_edge_index, new_values = adamic_adar(indexA=edge_index_a, valueA=values_a,
-                                                             indexB=edge_index_b, valueB=values_b,
-                                                             m=m, k=k, n=n,
-                                                             # sampling=edge_sampling,
-                                                             coalesced=True)
+                    new_edge_index, new_values = spspmm(indexA=edge_index_a, valueA=values_a,
+                                                        indexB=edge_index_b, valueB=values_b,
+                                                        m=m, k=k, n=n,
+                                                        # sampling=edge_sampling,
+                                                        coalesced=True)
 
                 if new_edge_index.size(1) == 0: continue
 
@@ -163,21 +164,40 @@ def join_edge_indexes(edge_index_dict_A: Dict[Tuple, Tuple[torch.Tensor]],
                 print(f"{e} \n {metapath_a}: {edge_index_a.max(1).values, values_a.shape}, "
                       f"{metapath_b}: {edge_index_b.max(1).values, values_b.shape}")
                 print("sizes: ", {"m": m, "k": k, "n": n, })
-                # raise e
+                raise e
                 continue
-
-        if metapaths and metapath_a in metapaths:
-            # In the current LATTE layer that calls this method, a metapath is repeated (i.e. not higher-order), so we return the edges to it again.
-            output_edge_index[metapath_a] = (edge_index_a, values_a)
 
     return output_edge_index
 
 
+def spspmm_outer_norm(indexA, valueA, indexB, valueB, m, k, n, coalesced=True, sampling=False):
+    A = SparseTensor(row=indexA[0], col=indexA[1], value=valueA,
+                     sparse_sizes=(m, k), is_sorted=not coalesced)
+    B = SparseTensor(row=indexB[0], col=indexB[1], value=valueB,
+                     sparse_sizes=(k, n), is_sorted=not coalesced)
+
+    deg_A = torch.pow(A.sum(1), -1)
+    deg_B = torch.pow(B.sum(1), -1)
+
+    diag_A = SparseTensor(row=torch.arange(deg_A.size(0), device=deg_A.device),
+                          col=torch.arange(deg_A.size(0), device=deg_A.device),
+                          value=deg_A,
+                          sparse_sizes=(deg_A.size(0), deg_A.size(0)), is_sorted=True)
+
+    diag_B = SparseTensor(row=torch.arange(deg_B.size(0), device=deg_B.device),
+                          col=torch.arange(deg_B.size(0), device=deg_B.device),
+                          value=deg_B,
+                          sparse_sizes=(deg_B.size(0), deg_B.size(0)), is_sorted=True)
+
+    out = A @ B
+    row, col, values = out.coo()
+
+    return torch.stack([row, col], dim=0), values
+
+
 def adamic_adar(indexA, valueA, indexB, valueB, m, k, n, coalesced=True, sampling=False):
-    if valueA.dim() > 1 and valueA.size(1) == 1:
-        valueA = valueA.squeeze(-1)
-    if valueB.dim() > 1 and valueB.size(1) == 1:
-        valueB = valueB.squeeze(-1)
+    assert valueA.dim() == 1
+    assert valueB.dim() == 1
 
     A = SparseTensor(row=indexA[0], col=indexA[1], value=valueA,
                      sparse_sizes=(m, k), is_sorted=not coalesced)
@@ -214,7 +234,7 @@ def edge_index2matrix(edge_index_dict, metapath, describe=True):
                                       edge_attr=edge_values)
 
     if describe:
-        print("sizes", st.sizes(), "max", st.storage.value().max(), "min", st.storage.value().min())
+        print(f"sizes {st.sizes()}, max-min: {(st.storage.value().max(), st.storage.value().min())}")
         axis = 0
         row_sum = st.sum(axis)  # [st.sum(axis).nonzero()]
         if row_sum.dim() > 2:
